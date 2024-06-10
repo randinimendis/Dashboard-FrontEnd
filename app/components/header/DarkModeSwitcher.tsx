985f74d663047b3a228adb34b444b80401fb0d5f
VidushaSanidu <vidushsanidu07@gmail.com>
2024-06-11 00:35:23 +0530

    use local storage hook

--- app/components/header/DarkModeSwitcher.tsx
+++ app/components/header/DarkModeSwitcher.tsx
@@ -6,7 +6,7 @@ const DarkModeSwitcher = () => {
   return (
     <li>
       <label
-        className={`relative m-0 block h-7.5 w-14 rounded-full ${
+        className={`relative m-0 block h-7 w-14 rounded-full ${
           colorMode === "dark" ? "bg-primary" : "bg-stroke"
         }`}
       >
@@ -17,7 +17,7 @@ const DarkModeSwitcher = () => {
               setColorMode(colorMode === "light" ? "dark" : "light");
             }
           }}
-          className="dur absolute top-0 z-50 m-0 h-full w-full cursor-pointer opacity-0"
+          className=" absolute top-0 z-50 m-0 h-full w-full cursor-pointer opacity-0"
         />
         <span
           className={`absolute left-[3px] top-1/2 flex h-6 w-6 -translate-y-1/2 translate-x-0 items-center justify-center rounded-full bg-white shadow-switcher duration-75 ease-linear ${
